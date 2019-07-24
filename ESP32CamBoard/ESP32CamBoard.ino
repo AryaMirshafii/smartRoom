ea278ab9bce7eb89bcc82e265a41290e70d022e7
Arya Mirshafii <amirshafii3@gatech.edu>
2019-07-23 21:34:03 -0400

    Ardunio is now able to handle pushbutton input and writes data to rom.

--- ESP32CamBoard/ESP32CamBoard.ino
+++ ESP32CamBoard/ESP32CamBoard.ino
@@ -1,18 +1,44 @@
-#include "esp_camera.h"
-#include <WiFi.h>
 #define CAMERA_MODEL_AI_THINKER
-
+#include "esp_camera.h"
 #include "camera_pins.h"
+#include <WiFi.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <EEPROM.h>
+#include <sstream>
+#include <string.h>
+#include <iomanip>
+#include <stdlib.h>
 
+#define EEPROM_SIZE 512
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 const char* ssid = "The Standard";
 
 void startCameraServer();
 
+const int CAMERA_PIN = 14;
+const String roomAddress = "https://gtoqgc0nbl.execute-api.us-east-1.amazonaws.com/dev/rooms";
+
+int RoomIdBase = 0;
+
+BLECharacteristic *pCharacteristic;
+
 void setup() {
+  RoomIdBase = sizeof(*ssid);
+  Serial.println("The size of ssid is" + RoomIdBase);
+  
+  if (EEPROM.begin(EEPROM_SIZE))
+  {
+    Serial.println("Intialized EEPROM");
+  }
+  pinMode(CAMERA_PIN,INPUT_PULLUP);
   Serial.begin(115200);
+  setUpBluetooth();
   Serial.setDebugOutput(true);
   Serial.println();
-
+  
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -73,6 +99,7 @@ void setup() {
 #endif
 
   WiFi.begin(ssid);
+  
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -94,6 +121,74 @@ void loop() {
     Serial.println("Error With wifi ");
     
   }
-  Serial.println("Running");
-  delay(10000);
+
+  if(digitalRead(CAMERA_PIN) == LOW){
+    Serial.println("Updating room");
+  }
+  
+  checkRoomId();
+  delay(1000);
 }
+
+void setUpBluetooth(){
+  BLEDevice::init("SmartRoom");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+}
+
+void updateRoom(){
+  
+}
+
+void checkRoomId(){
+  const char* bluetoothData = pCharacteristic->getValue().c_str();
+  //Serial.println(bluetoothData);
+  String writtenData = getRoomId();
+  writeRoomId(bluetoothData);
+  Serial.println("Written data is" + writtenData);
+}
+
+void writeRoomId(String data){
+  int _size = data.length();
+  int i;
+  for(i=0;i<_size;i++)
+  {
+    //Serial.println("writing " + data[i]);
+    EEPROM.write(RoomIdBase+i,data[i]);
+  }
+  EEPROM.write(RoomIdBase+_size,'\0');
+  EEPROM.commit();
+  Serial.println("wrote: " + data);
+}
+
+String getRoomId(){
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(RoomIdBase);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(RoomIdBase+len);
+    data[len]=k;
+    len++;
+    //Serial.println("reading " + k);
+  }
+  data[len]='\0';
+  return String(data);
+}
+
